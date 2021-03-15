fc0208de30dde9c3def2f38a5e2d55d1a5c52ab7
Adrian Alvarado <dev.adrian.alvarado@gmail.com>
2021-03-15 14:29:38 -0600

    new push

---
+++ backend/data.js
@@ -0,0 +1,119 @@
+import bcrypt from 'bcryptjs';
+
+const data = {
+    users: [
+        {
+          name: 'Basir',
+          email: 'admin@example.com',
+          password: bcrypt.hashSync('1234', 8),
+          isAdmin: true,
+        },
+        {
+          name: 'John',
+          email: 'user@example.com',
+          password: bcrypt.hashSync('1234', 8),
+          isAdmin: false,
+        },
+      ],
+    products: [
+        {
+            _id:'1',
+            name:'Fuente de luz VSL 55',
+            category: 'Hospital',
+            image:'/images/product1.jpg',
+            brand:'VSL',
+            rating:4.8,
+            numReviews:10,
+            description: 'High quality product'
+        },
+        {
+            _id:'2',
+            name:'Bomba de Infusion',
+            category: 'Hospital',
+            image:'/images/product2.jpg',
+            brand:'VSL',
+            rating:4.8,
+            numReviews:10,
+            description: 'High quality product'
+        },
+        {
+            _id:'3',
+            name:'Concentrador de Oxigeno',
+            category: 'Hospital',
+            image:'/images/product3.jpg',
+            brand:'VSL',
+            rating:4.8,
+            numReviews:10,
+            description: 'High quality product'
+        },
+        {
+            _id:'4',
+            name:'Tococardiografo',
+            category: 'Hospital',
+            image:'/images/product4.jpg',
+            brand:'VSL',
+            rating:4.8,
+            numReviews:10,
+            description: 'High quality product'
+        },
+        {
+            _id:'5',
+            name:'Monitor multiparametros C50',
+            category: 'Hospital',
+            image:'/images/product5.jpg',
+            brand:'VSL',
+            rating:4.8,
+            numReviews:10,
+            description: 'High quality product'
+        },
+        {
+            _id:'6',
+            name:'Electrocardiografo 12 canales',
+            category: 'Hospital',
+            image:'/images/product6.jpg',
+            brand:'VSL',
+            rating:4.8,
+            numReviews:10,
+            description: 'High quality product'
+        },
+        {
+            _id:'7',
+            name:'Exilis Elite',
+            category: 'Spa',
+            image:'/images/product7.jpg',
+            brand:'VSL',
+            rating:4.8,
+            numReviews:10,
+            description: 'High quality product'
+        },
+        {
+            _id:'8',
+            name:'Electrocardiografo Touch 12 canales',
+            category: 'Hospital',
+            image:'/images/product8.jpg',
+            brand:'VSL',
+            rating:4.8,
+            numReviews:10,
+            description: 'High quality product'
+        }
+    ],
+    promotions: [
+        {
+            _id:'1',
+            category: 'Hospital',
+            image:'/images/promo1.jpg',
+        },
+        {
+            _id:'2',
+            category: 'Veterinaria',
+            image:'/images/promo2.jpg',
+        },
+        {
+            _id:'3',
+            category: 'Spa',
+            image:'/images/promo3.jpg',
+        }
+    ]
+};
+
+export default data;
