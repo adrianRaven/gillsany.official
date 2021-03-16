c9e1661a3a57c2403da9ddac980b4483b502e103
Adrian Alvarado <dev.adrian.alvarado@gmail.com>
2021-03-15 18:03:04 -0600

    changes

--- backend/server.js
+++ backend/server.js
@@ -24,10 +24,10 @@ app.get("/api/products", (req, res) => {
   res.send(data.products);
 });
 
-app.use('/api/users', userRouter);
-app.get("/", (req, res) => {
-  res.send("Server is ready");
-}); 
+app.use(express.static(path.join(__dirname, '/frontend/build')));
+app.get('*', (req, res) =>
+  res.sendFile(path.join(__dirname, '/frontend/build/index.html'))
+);
 
 app.use((err, req, res, next) => {
   res.status(500).send({ message: err.message });
