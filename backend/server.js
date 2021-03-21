693b435d3fd89b8296d37c183e81357dbef43933
Adrian Alvarado <dev.adrian.alvarado@gmail.com>
2021-03-20 22:34:45 -0600

    path import

--- backend/server.js
+++ backend/server.js
@@ -1,6 +1,7 @@
 import express from "express";
 import data from "./data.js";
 import mongoose from 'mongoose';
+import path from 'path';
 import userRouter from './routers/userRouter.js';
 
 const app = express();
@@ -24,11 +25,12 @@ app.get("/api/products", (req, res) => {
   res.send(data.products);
 });
 
-app.use(express.static(path.join(__dirname, '/front/build')));
+const __dirname = path.resolve();
+app.use('/uploads', express.static(path.join(__dirname, '/uploads')));
+app.use(express.static(path.join(__dirname, '/frontend/build')));
 app.get('*', (req, res) =>
-  res.sendFile(path.join(__dirname, '/front/build/index.html'))
+  res.sendFile(path.join(__dirname, '/frontend/build/index.html'))
 );
-
 app.use((err, req, res, next) => {
   res.status(500).send({ message: err.message });
 });
